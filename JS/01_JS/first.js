16fb95330993af4a3b070cdb07f86f3838ee4c2c
Derrick Abuga <152125688+derrickAbuga06@users.noreply.github.com>
2023-12-27 21:29:14 +0300

    Modified: 01_JS\first.js

--- JS/01_JS/first.js
+++ JS/01_JS/first.js
@@ -2,3 +2,22 @@
 var myVar;
 myVar = "Hello, World!";
 console.log(myVar);
+
+var myNum;
+var myNum2;
+myNum = 14;
+myNum2 = 24;
+
+var myStr = `The sum of 14 and 24 is...${14 + 24}`;
+console.log(myStr);
+
+function nl() {
+  console.log("\n")
+}
+nl()
+
+//var userInput = prompt("Hello? ");
+
+/*function user() {
+  if 
+}*/
